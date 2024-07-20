d4323210067a65ddde87042724558aa29f8d25a6
robjlyons <81177492+robjlyons@users.noreply.github.com>
2024-07-20 22:05:42 +0100

    Create qlearning.asm

---
+++ qlearning.asm
@@ -0,0 +1,12 @@
+; File: qlearning.asm
+
+IF !DEF(QLEARNING_ASM)
+QLEARNING_ASM EQU 1
+
+SECTION "Q-Learning Variables", WRAM0
+
+wQTableInitialized:: db
+wCurrentState:: db
+wQTable:: ds 4 * 4 ; 4 moves, 4 states, 1 byte each
+
+ENDC
