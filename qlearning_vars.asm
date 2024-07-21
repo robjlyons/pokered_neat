ac52e158de8ceb981b7dc8a7f240b127345ee3f0
robjlyons <81177492+robjlyons@users.noreply.github.com>
2024-07-21 20:26:29 +0100

    Update qlearning_vars.asm

--- qlearning_vars.asm
+++ qlearning_vars.asm
@@ -3,8 +3,3 @@
 INCLUDE "qlearning.asm"
 
 SECTION "Q-Learning Variables Definitions", WRAM0
-
-DEF QLEARNING_VARS_DEFINED EQU 1
-wQTableInitialized:: db
-wCurrentState:: db
-wQTable:: ds 4 * 4 ; 4 moves, 4 states, 1 byte each
