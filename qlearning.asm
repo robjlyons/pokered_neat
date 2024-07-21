af2e439c05bb2e80d66c8ada9815bb69cff91495
robjlyons <81177492+robjlyons@users.noreply.github.com>
2024-07-21 20:19:27 +0100

    Update qlearning.asm

--- qlearning.asm
+++ qlearning.asm
@@ -1,12 +1,12 @@
 ; File: qlearning.asm
 
 IF !DEF(QLEARNING_ASM)
-DEF QLEARNING_ASM EQU 1
+QLEARNING_ASM EQU 1
 
 SECTION "Q-Learning Variables", WRAM0
 
 IF !DEF(QLEARNING_VARS_DEFINED)
-DEF QLEARNING_VARS_DEFINED EQU
+QLEARNING_VARS_DEFINED EQU 1
 wQTableInitialized:: db
 wCurrentState:: db
 wQTable:: ds 4 * 4 ; 4 moves, 4 states, 1 byte each
