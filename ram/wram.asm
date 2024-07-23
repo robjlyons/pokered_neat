3822111fa164324cbf8d2d915a2243b11cc8815c
robjlyons <81177492+robjlyons@users.noreply.github.com>
2024-07-23 20:34:01 +0100

    Update wram.asm

--- ram/wram.asm
+++ ram/wram.asm
@@ -1667,11 +1667,12 @@ wSavedNPCMovementDirections2Index:: db
 
 wPlayerName:: ds NAME_LENGTH
 
-wRandomSeed: db
+SECTION "Battle WRAM", WRAM0
 
-wQTable: ds 4 * 4 ; 4 moves, 4 states, 1 byte each
-wQTableInitialized: db
-wCurrentState: db
+wRandomSeed:: dw
+wQTable:: ds 4 * 4 ; 4 moves, 4 states, 1 byte each
+wQTableInitialized:: db
+wCurrentState:: db
 
 SECTION "Party Data", WRAM0
 
